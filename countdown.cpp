451ed2700cc57f2700a90939bd1fa2e6b7b86497
Kasun Gimantha <97676779+KasunGimantha@users.noreply.github.com>
2022-08-01 15:24:03 +0530

    Create countdown.cpp

---
+++ countdown.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+ 
+    
+for(int i=n ; i>0 ; i--){
+    
+    if(i%5){
+    cout << i << endl;
+    }
+    else{
+        cout<<"beep"<<endl;
+    }
+    
+  
+} 
+    
+    
+    return 0;
+}
